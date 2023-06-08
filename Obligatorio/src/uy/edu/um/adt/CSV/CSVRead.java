31ddeb4df2bd63417ee09e3f98c51728612c4f34
joaco0o0 <jheredero@correo.um.edu.uy>
2023-06-08 11:50:11 -0300

    reader csv

--- Obligatorio/src/uy/edu/um/adt/CSV/CSVRead.java
+++ Obligatorio/src/uy/edu/um/adt/CSV/CSVRead.java
@@ -6,38 +6,24 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import uy.edu.um.adt.ENTITIES.Tweet;
-import uy.edu.um.adt.TADS.MyLinkedList.Mylist;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import uy.edu.um.adt.TADS.MyLinkedList.MyLinkedList;
 
 public class CSVRead {
     private static final String Ruta_Archivo = "Obligatorio/src/uy/edu/um/adt/CSV/prueba.csv";
 
     public static void main(String[] args) {
-        List<Tweet> tweetList = new Mylist<>();
+        MyLinkedList<Tweet> tweetList = new MyLinkedList<>();
 
         try {
             Reader reader = Files.newBufferedReader(Paths.get(Ruta_Archivo));
             CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT);
             for (CSVRecord record : parser) {
-                Tweet tweet = new Tweet();
-                tweet.setTweetNumber(record.get(0));
-                tweet.setUserName(record.get(1));
-                tweet.setUserDescription(record.get(2));
-                tweet.setUserCreated(record.get(3));
-                tweet.setUserFollowers(record.get(4));
-                tweet.setUserFriends(record.get(5));
-                tweet.setUserFavourites(record.get(6));
-                tweet.setUserVerified(record.get(7));
-                tweet.setDate(record.get(8));
-                tweet.setText(record.get(9));
-                tweet.setHashtags(record.get(10));
-                tweet.setSource(record.get(11));
-                tweet.setIsRetweet(record.get(12));
-
+                Tweet tweet = new Tweet(record.get(0),record.get(1),record.get(2),record.get(3),record.get(4),record.get(5),record.get(6),record.get(7),record.get(8),record.get(9),record.get(10),record.get(11),record.get(12));
                 tweetList.add(tweet);
             }
 
--- Obligatorio/src/uy/edu/um/adt/ENTITIES/Tweet.java
+++ Obligatorio/src/uy/edu/um/adt/ENTITIES/Tweet.java
@@ -6,15 +6,81 @@ public class Tweet {
     private String userName;
     private String userDescription;
     private int userCreated;
-    private String userFollowers;
-    private String userFriends;
-    private String userFavourites;
-    private String userVerified;
+    private int userFollowers;
+    private int userFriends;
+    private long userFavourites;
+    private boolean userVerified;
     private String date;
     private String text;
     private String hashtags;
     private String source;
-    private String isRetweet;
+    private boolean isRetweet;
 
-
+    public Tweet(String tweetNumber, String userName, String userDescription, String userCreated, String userFollowers, String userFriends, String userFavourites, String userVerified, String date, String text, String hashtags, String source, String isRetweet) {
+        this.tweetNumber = Integer.parseInt(tweetNumber);
+        this.userName = userName;
+        this.userDescription = userDescription;
+        this.userCreated = Integer.parseInt(userCreated);
+        this.userFollowers = Integer.parseInt(userFollowers);
+        this.userFriends = Integer.parseInt(userFriends);
+        this.userFavourites = Long.parseLong(userFavourites);
+        this.userVerified = Boolean.parseBoolean(userVerified);
+        this.date = date;
+        this.text = text;
+        this.hashtags = hashtags;
+        this.source = source;
+        this.isRetweet = Boolean.parseBoolean(isRetweet);
     }
+
+    public int getTweetNumber() {
+        return tweetNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserDescription() {
+        return userDescription;
+    }
+
+    public int getUserCreated() {
+        return userCreated;
+    }
+
+    public int getUserFollowers() {
+        return userFollowers;
+    }
+
+    public int getUserFriends() {
+        return userFriends;
+    }
+
+    public long getUserFavourites() {
+        return userFavourites;
+    }
+
+    public boolean isUserVerified() {
+        return userVerified;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getHashtags() {
+        return hashtags;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public boolean isRetweet() {
+        return isRetweet;
+    }
+}
