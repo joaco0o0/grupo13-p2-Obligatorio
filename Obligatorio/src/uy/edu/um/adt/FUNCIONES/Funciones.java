a0ced539d274b4615aa3fc81b31d45fbe1c655f9
TOMYKS <tgonzalez1@correo.um.edu.uy>
2023-06-24 17:58:11 -0300

    interface falta el de 1 3 5 e implemenraciones

---
+++ Obligatorio/src/uy/edu/um/adt/FUNCIONES/Funciones.java
@@ -0,0 +1,13 @@
+package uy.edu.um.adt.FUNCIONES;
+
+import uy.edu.um.adt.ENTITIES.Hashtag;
+import uy.edu.um.adt.TADS.MyLinkedList.Mylist;
+
+public interface Funciones {
+    //Funcion 1
+    Mylist<String> usuariosMasTwits();//Funcion 2
+    //Funcion 3
+    Hashtag hashtagMasUsado();//Funcion 4
+    //Funcion 5
+    int cantidadDeTweetsPalabraFrase();//Funcion 6
+}
---
+++ Obligatorio/src/uy/edu/um/adt/FUNCIONES/FuncionesImpl.java
@@ -0,0 +1,21 @@
+package uy.edu.um.adt.FUNCIONES;
+
+import uy.edu.um.adt.ENTITIES.Hashtag;
+import uy.edu.um.adt.TADS.MyLinkedList.Mylist;
+
+public class FuncionesImpl implements Funciones{
+    @Override
+    public Mylist<String> usuariosMasTwits() {
+        return null;
+    }
+
+    @Override
+    public Hashtag hashtagMasUsado() {
+        return null;
+    }
+
+    @Override
+    public int cantidadDeTweetsPalabraFrase() {
+        return 0;
+    }
+}
